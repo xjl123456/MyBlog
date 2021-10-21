a513a88ef6a846a9773ea304993153217fa324a4
xjl123456 <32998115+xjl123456@users.noreply.github.com>
2021-10-21 17:26:38 +0800

    Update register_controller.go

--- controllers/register_controller.go
+++ controllers/register_controller.go
@@ -3,9 +3,11 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"fmt"
 )
 
 func RegisterGet(c *gin.Context) {
 	//返回html
+	fmt.Println("hello")
 	c.HTML(http.StatusOK, "register.html", gin.H{"title": "注册页"})
 }
